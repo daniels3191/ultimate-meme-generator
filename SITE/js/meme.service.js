856b1cc7fac5f3ebebbf2ce9625869232af92482
Daniel Sahar <daniel.sahar1@gmail.com>
2026-01-02 12:09:35 +0200

    Add render images

---
+++ SITE/js/meme.service.js
@@ -0,0 +1,30 @@
+'use strict'
+
+
+// var gImgs = [{id: 1, url: 'img/1.jpg', keywords: ['funny', 'humman']}]
+var gMeme = {
+selectedImgId: 1,
+selectedLineIdx: 0,
+lines: [
+{
+txt: 'I sometimes eat Falafel'
+,
+size: 20,
+color: 'red'
+}
+]
+}
+// var gKeywordSearchCountMap = {'funny': 12,'cat': 16, 'baby': 2}
+
+function getMeme(){
+    console.log('test');
+    
+    return gMeme
+}
+
+function setLineTxt(text){
+
+    gMeme.lines[0].txt = text
+    
+
+}
